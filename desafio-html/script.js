5192f2fb00a97387ae86007df6e96a3dafb7baef
jkassner <julianokassner@hotmail.com>
2020-04-19 10:52:40 -0300

    ajustes pop-up, form

--- desafio-html/script.js
+++ desafio-html/script.js
@@ -7,13 +7,26 @@ function scrollPage(id) {
 
 }
 
-function enviarmsg(form) {
-    debugger;
-    modal = document.getElementById('container-modal');
+function enviarmsg(button) {
+    var frase = getMsgDefault();
+    var modal = document.getElementById('container-modal');
+    var contentModal = document.querySelector('.content-modal');
+    var nome = button.form.elements.nome.value;
+
     modal.style.display = 'flex';
+    document.body.style['max-width'] = '100%';
+    frase = frase.replace('{nome}', nome);
+    contentModal.innerText = frase;
 }
 
 function closeModal() {
-    modal = document.getElementById('container-modal');
-    modal.style.display = 'none'; 
+    var modal = document.getElementById('container-modal');
+    modal.style.display = 'none';
+    document.body.style['max-width'] = '900px';
+    document.querySelector('.content-modal').innerText = ''; 
+}
+
+function getMsgDefault() {
+    return 'Mensagem enviado com sucesso {nome}.\n'+
+    'Muito obrigado pelo contato.';
 }
